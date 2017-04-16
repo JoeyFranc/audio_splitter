Added second order derivative for the exponential function. Fixed the one unit ICA function to take in the previous components, so that it can be decorrelated in the loop there, and not in the multi unit function. Added functionality in the inout file for fake signals, taken very heavily from the Scipy doc example.

FastICA.py
@@ -4,7 +4,7 @@ import preprocessing as prep
 import functions as f
 import inout
 
-def one_unit_ica(X, g, dg):
+def one_unit_ica(X, g, dg, w_prev = None):
 
     w = np.random.random(X.shape[1])
     w_old = np.zeros(w.shape)
@@ -15,6 +15,14 @@ def one_unit_ica(X, g, dg):
         exp2 = sum(dg(np.dot(w,x)) for x in X)/len(X) * w
 
         w = exp1 - exp2
+
+
+        #If we are doing multi_unit)_ica, need to decorrelate the vectors
+        if w_prev != None:
+            assert(type(w_prev == list))
+            w -= sum([np.dot(w.T, wj) * wj for wj in w_prev])
+
+        #normalize
         w = w/np.linalg.norm(w)
 
         if np.allclose(abs(np.dot(w, w_old)), 1):
@@ -27,12 +35,27 @@ def one_unit_ica(X, g, dg):
 
 def multi_unit_ica(X, g, dg, num_components):
     W = [0]*num_components #'empty' list to hold each weight vector
-    for n in range(num_components):
-        W[n] = one_unit_ica(X, g, dg) #independent component
-        W[n] = W[n] - sum(np.dot(W[n], w)*w for w in W[:n]) #decorrelate
-        W[n] = W[n] / np.linalg.norm(W[n])
+    W[0] = one_unit_ica(X, g, dg)
+
+    for n in range(1, num_components):
+        W[n] = one_unit_ica(X, g, dg, W[:n]) #independent component
 
     return W
+
+
+
+def trial():
+    S = inout.DGP()
+    S.set_sources()
+    S = S.get_mixed()
+
+    num_components = S.shape[1]
+    mean, centered = prep.centering(S)
+    whitened = prep.whitening(centered)
+
+    W = np.array(multi_unit_ica(S, f.dexp, f.d2exp, num_components))
+    return W, S
+
 
 def ica(fn):
 # Runs ica on a file named "fn"
@@ -50,7 +73,8 @@ def ica(fn):
         processed = prep.whitening(centered)
     
         # Second, run ICA
-        sources += [multi_unit_ica(processed, f.exp, f.dexp, 3)]
+        #Note: Using first and second derivatives of G
+        sources += [multi_unit_ica(processed, f.dexp, f.d2exp, 3)]
 
     # Concatenate channels
     for channel in sources[1:]:

inout.py
@@ -1,8 +1,9 @@
 import wave as wav
 import numpy as np
 import scipy.io.wavfile as sciowav
+from scipy import signal
 
-
+np.random.seed(0)
 
 """ Public Methods """
 
@@ -101,9 +102,52 @@ class _sign_extend():
         # Positive case
         return integer
 
+
+
+class DGP():
+    def __init__(self, num_points = 2000):
+        self.num_points = num_points
+        self.time = np.linspace(0, 8, num_points)
+
+    def set_sources(self):
+        self.sources = _unmixed_sources(self.time)
+
+    def get_mixed(self):
+        return _mix_sources(self.sources)
+
+
+
+    #private
+def _unmixed_sources(time = 2000):
+    s1 = np.sin(2 * time)
+    s2 = np.sign(np.sin(3 * time))
+    s3 = signal.sawtooth(2 * np.pi * time)
+
+    S = np.c_[s1, s2, s3]
+    S += 0.2 * np.random.normal(size=S.shape)
+    S /= S.std(axis=0) #Standardize the data
+
+    return S
+
+def _mix_sources(sources):
+    A = np.array([[1, 1, 1], [0.5, 2, 1.0], [1.5, 1.0, 2.0]])
+    X = np.dot(sources, A.T)
+    return X
+
+
+
 if __name__ == '__main__':
 
     io = WavIO('elvis_riverside.wav')
     channels = io.read_source()
     channels = [ channels[0][:int(len(channels[0])/2)] ]
     io.write_sources(channels)
+
+
+
+
+
+
+
+
+